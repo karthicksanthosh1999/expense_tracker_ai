e145b4fed916febb51078be2322acc91f56e9dfb
karthicksanthosh <karthick.santhosh1999@gmail.com>
2026-04-06 23:32:17 +0530

    Completed the landing page section

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import HeroSection from "@/components/hero-section";
-import { featuresData, statsData } from "@/data/landing";
+import { featuresData, howItWorksData, statsData, testimonialsData } from "@/data/landing";
 import {
   Card,
   CardAction,
@@ -9,6 +9,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -34,14 +37,14 @@ export default function Home() {
       <section className="py-20">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-12">Everything you need to manage your finances</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg: grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {
               featuresData.map((feature, index) => (
-                <Card>
-                  <CardContent>
+                <Card key={index} className={'p-6'}>
+                  <CardContent className="space-y-4 pt-4">
                     {feature.icon}
-                    <h3>{feature.title}</h3>
-                    <h3>{feature.description}</h3>
+                    <h3 className="text-xl font-semibold">{feature.title}</h3>
+                    <h3 className="text-gray-600">{feature.description}</h3>
                   </CardContent>
                 </Card>
               ))
@@ -49,6 +52,65 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section className="py-20 bg-blue-50">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-12">How it works</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {
+              howItWorksData.map((step, index) => (
+                <div key={index} className="text-center">
+                  <div className="w-16 h-16 bg-blue-100 rounded-full flex items-center justify-center mx-auto mb-6">
+                    {step.icon}
+                  </div>
+                  <h3 className="text-xl font-semibold mb-4">{step.title}</h3>
+                  <p className="text-gray-600">{step.description}</p>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-12">What Our Users Say</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {
+              testimonialsData.map((testimonials, index) => (
+                <Card key={index} className={'p-6'}>
+                  <CardContent className="space-y-4 pt-4">
+                    <div className="flex items-center mb-4">
+                      <Image
+                        src={testimonials.image}
+                        alt={testimonials.name}
+                        width={40}
+                        height={40}
+                        className="rounded-lg"
+                      />
+                      <div className="ml-4">
+                        <div className="font-semibold">{testimonials.name}</div>
+                        <div className="text-sm text-gray-600">{testimonials.role}</div>
+                      </div>
+                    </div>
+                    <p className="text-gray-600">{testimonials.quote}</p>
+                  </CardContent>
+                </Card>
+              ))
+            }
+          </div>
+        </div>
+      </section>
+
+
+      <section className="py-20 bg-blue-600">
+        <div className="container mx-auto px-4 text-center">
+          <h2 className="text-3xl font-bold text-white mb-4">Ready to Take Control of Your Finances?</h2>
+          <p className="text-blue-100 mb-8 max-w-2xl mx-auto"> Join thousands of users who are already managing their finances master with Welth</p>
+          <Link href={'/dashboard'}>
+            <Button size="lg" className={'bg-white text-blue-600 hover:bg-blue-50 animate-bounce cursor-pointer'}>Start Free Trial</Button></Link>
+        </div>
+      </section>
+
     </div>
   );
 }
